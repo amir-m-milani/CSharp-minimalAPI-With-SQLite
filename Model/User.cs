a4bca65a41917641ffb6b052144cda0a6104af0b
amir-m-milani <amirmilani08@gmail.com>
2024-08-13 14:30:00 +0330

    add login register dto . User model

---
+++ Model/User.cs
@@ -0,0 +1,8 @@
+namespace Athenticate.Model;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
