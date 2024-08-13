2792a618f35fb5799b22b0adfd25cfb01eebfee4
amir-m-milani <amirmilani08@gmail.com>
2024-08-13 14:43:42 +0330

    add created date to user

--- Model/User.cs
+++ Model/User.cs
@@ -5,4 +5,5 @@ public class User
     public string Name { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
+    public DateTime CreatedDate { get; set; }
 }
